547805ed9048607085f6bd638ab69e63883a54b6
Weston Woodard <weston.woodard@slalom.com>
2022-02-22 12:10:08 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // the html tag used to embed your component
   selector: 'app-root',
+  // path to html file
   templateUrl: './app.component.html',
+  // path to styling file(s)
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-directory-app';
+  // methods and properties go here
 }
